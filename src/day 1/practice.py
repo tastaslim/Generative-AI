71a1548dd494f316d834351501bee529b93c2904
Taslim <taslim.arif@druva.com>
2026-04-29 09:35:23 +0530

    Starting Gen AI Tutorial

--- src/day 1/practice.py
+++ src/day 1/practice.py
@@ -1,8 +1,8 @@
-from tiktoken import Encoding, encoding_for_model
-inputText: str = "Who is the prime minister of India?"
+from typing import List
+from tiktoken import encoding_for_model, Encoding
+inputText: str = "Who is the Captain of RCB?"
 tokenizer: Encoding = encoding_for_model(model_name="gpt-5")
-tokens: list[int] = tokenizer.encode(inputText)
+tokens: List[int] = tokenizer.encode(inputText)
 print(tokens)
-
 originalText: str = tokenizer.decode(tokens)
 print(originalText)
