9479f1596d8da87b1a31a00dbef0e7268591483f
Taslim <taslim.arif@druva.com>
2026-04-30 01:37:43 +0530

    Day 3: Added Langchain Dynamic Prompts and Chaining tutorial

---
+++ src/day3/llm_QnA_chatbot.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+
+
+def ChatBotQA(llmProvider: ChatGroq):
+    while True:
+        userInput: str = input("User:")
+        if userInput in ("quit", "bye", "close"):
+            print("Goodbye 👋")
+            break
+        output = llmProvider.invoke(userInput)
+        print(f"AI: {output.content}")
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    groqLLM: ChatGroq = ChatGroq(model="llama-3.3-70b-versatile")
+    ChatBotQA(groqLLM)
